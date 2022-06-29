ef3eb959646b8c92f404fee16146c0a4fe8a805d
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2022-06-29 14:43:21 -0300

    Adicionado exemplo de useEffect e useState

--- estudando-hooks/src/views/examples/UseState.jsx
+++ estudando-hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
+
+
+    const [count, setCount] = useState(0)
+
+    const [name, setName] = useState('Digite seu nome!')
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+
+            <SectionTitle title='Exercício #01'></SectionTitle>
+
+            <div className="center">
+
+                <span className="text">{count}</span>
+                <div>
+
+                    <button className="btn" onClick={() => setCount(0) }>ZERAR</button>
+                    <button className="btn" onClick={() => setCount(count - 1) }>-1</button>
+                    <button className="btn" onClick={() => setCount(count + 1) }>+1</button>
+                    <button className="btn" onClick={() => setCount(current => current + 1000) }>+1000 </button>
+                    
+                </div>
+
+            </div>
+
+            <SectionTitle title='Exercício #02 '></SectionTitle>
+                <input  
+                    className='input' 
+                    type="text" 
+                    value={name}
+                    onChange={e=>setName(e.target.value)} />
         </div>
     )
 }
